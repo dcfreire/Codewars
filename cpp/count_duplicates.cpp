59e60cf4abdb9b6c7b14b50097d30d55691685c5
toodamfilthy <dcfreire77@gmail.com>
2019-05-09 22:41:50 -0300

    a

---
+++ cpp/count_duplicates.cpp
@@ -0,0 +1,33 @@
+size_t duplicateCount(const std::string& in); // helper for tests
+#include <string.h>
+
+size_t duplicateCount(const char* in)
+{
+    int c = 0;
+    bool done = false;
+    int len = strlen(in);
+
+    char a[len];
+    for(int i = 0; i < strlen(in); i++){
+      a[i] = in[i];
+    }
+
+    for(int i = 0; i < strlen(in); i++){
+      for(int f = i+1; f < strlen(in); f++){
+        if(a[i] == (char)NULL)
+          break;
+        if((int)a[f] == (int)a[i] || (int)a[f] == (int)a[i]-32 || (int)a[f]-32 == (int)a[i] ){
+          if(!done)
+            c++;
+
+          a[f] = (char)NULL;
+
+          done = true;
+          }
+
+      }
+      done = false;
+    }
+
+    return c;
+}
