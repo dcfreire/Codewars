181920590f40a638a3d90cd35961cda8bc4791d6
shi <dc-freire@hotmail.com>
2019-05-11 12:30:59 -0300

    '

---
+++ cpp/convergence.cpp
@@ -0,0 +1,14 @@
+class Suite
+{
+public:
+  static double going(int n){
+    double ret = 1, prev = 1, scale = 0.000001;
+    for(int i = 0; i < n-1; i++){
+      prev /= n - i;
+      ret += prev;
+    }
+    ret = (int)(ret/scale) * scale;
+    return ret;
+
+  }
+};
