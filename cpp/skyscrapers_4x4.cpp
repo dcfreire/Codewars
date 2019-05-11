1fb9cfbbe83e80daa29918c5b3e8f89f1d68ed2b
shi <dc-freire@hotmail.com>
2019-05-11 11:09:28 -0300

    a

---
+++ cpp/skyscrapers_4x4.cpp
@@ -0,0 +1,19 @@
+
+
+
+
+
+int** SolvePuzzle (int *clues) {
+  int **ret = new int[4][4];
+  bool notf = true;
+  int c = 0;
+  while(notf){
+
+
+
+    c++;
+    if(c>15)
+      c= 0;
+  }
+  return 0;
+}
---
+++ cpp/zeros.cpp
@@ -0,0 +1,10 @@
+
+#include<math.h>
+
+long zeros(long n) {
+  int ret = 0;
+  for(int i = 1; i <= (int)(log(n)/log(5)); i++)
+    ret += (int)(n/pow(5, i));
+
+  return ret;
+}
