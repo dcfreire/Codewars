06f26ea444d5eacad449d4e30b8b947d50881c47
shi <dc-freire@hotmail.com>
2019-05-13 20:35:41 -0300

    '

--- cpp/decoding_morse_1.cpp
+++ cpp/decoding_morse_1.cpp
@@ -1,17 +1,25 @@
+void remove_leading(std::string &str){
+  while(str[str.length()-1] == ' ')
+    str.erase(str.length()-1, 1);
+
+}
+
 std::string decodeMorse(std::string morseCode) {
         // ToDo: Accept dots, dashes and spaces, return human-readable message
-        std::string decoded, dd = "";
+        std::string decoded = "", dd = "";
         while(morseCode.length() != 0) {
                 while(morseCode[0] != ' ' && morseCode.length() != 0) {
                         dd += morseCode[0];
                         morseCode.erase(0, 1);
                 }
                 if(morseCode.length() != 0) {
-                        if(morseCode[0] = ' ') {
-                                morseCode.erase(0, 3);
-                                decoded += MORSE_CODE[ dd ] + " ";
+                morseCode.erase(0, 1);
+
+                        if(morseCode[0] == ' ') {
+                                morseCode.erase(0, 2);
+                                if(dd.length() > 0)
+                                  decoded += MORSE_CODE[ dd ] + " ";
                         }else{
-                                morseCode.erase(0, 1);
                                 decoded += MORSE_CODE[ dd ];
                         }
                 }else{
@@ -19,5 +27,6 @@ std::string decodeMorse(std::string morseCode) {
                 }
                 dd.clear();
         }
+        remove_leading(decoded);
         return decoded;
 }
