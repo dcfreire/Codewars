bfe0ed5ae5cbe06f3a85feb061abf2b034a9b7c7
toodamfilthy <dcfreire77@gmail.com>
2019-05-14 19:08:44 -0300

    Terminei nu

--- cpp/lcs_peform.cpp
+++ cpp/lcs_peform.cpp
@@ -5,49 +5,38 @@
 #include <map>
 using namespace std;
 
-void update_table(vector<vector<int> > &tab, int x, int y, char ch, string b){
-        int a = tab[x][y];
-        bool first = true;
-        for(int i = x; i < tab.size(); i++)
-                for(int j = y; j < tab[0].size(); j++){
-                        if((tab[i][j]) < (a+1)){
-                          if(b[j] != ch || first){
-                            tab[i][j]++;
-                            first = false;
-                          }
-                        }
-                        first = true;
-                      }
+void update_table(vector<vector<int> > &tab, string x, string y){
 
+      int	m = x.length();
+	    int  n = y.length();
+
+	for (int i = 1; i < m; i++){
+	   for (int j = 1; j < n; j++){
+	      if (x[i] == y[j])
+	       tab[i][j] = tab[i-1][j-1] + 1;
+	        else
+            if (tab[i-1][j] >= tab[i][j-1])
+	             tab[i][j]=tab[i-1][j];
+	              else
+                  tab[i][j]=tab[i][j-1];
+}
+}
 }
 
 string lcs(const string& x, const string& y){
-        int n = 0, c = y.length()-1, up, left, here;
+        int c = y.length()-1, up, left, here;
         cout << "x: " << x << endl;
         cout << "y: " << y<<endl;
 
         string ret = "";
-        vector<vector<int> > mat(x.length(), vector<int>(y.length(), 0));
+        vector<vector<int> > mat(x.length() + 1, vector<int>(y.length() + 1, 0));
         if(!x.length() || !y.length())
           return "";
 
 
-        for(int i = 0; i < x.length(); i++) {
-                for(int j = 0; j < y.length(); j++) {
-                        if(x[i] == y[j]){
-                          for(int i = 0; i < x.length(); i++) {
-                                  for(int j = 0; j < y.length(); j++) {
-                                          cout << " " <<mat[i][j];
-                                  }
-                                  cout << endl;
-                          }
-                          cout << endl;
 
-                                update_table(mat, i, j, y[j], y);
-                              }
+        update_table(mat, x, y);
 
-                }
-        }
         for(int i = 0; i < x.length(); i++) {
                 for(int j = 0; j < y.length(); j++) {
                         cout << " " <<mat[i][j];
