028a786ccbb4231f536dc3223be9eaad88a9be5d
shi <dc-freire@hotmail.com>
2019-05-14 22:13:33 -0300

    '

--- cpp/lcs_peform.cpp
+++ cpp/lcs_peform.cpp
@@ -7,20 +7,20 @@ using namespace std;
 
 void update_table(vector<vector<int> > &tab, string x, string y){
 
-      int	m = x.length();
-	    int  n = y.length();
+        int m = x.length();
+        int n = y.length();
 
-	for (int i = 1; i < m; i++){
-	   for (int j = 1; j < n; j++){
-	      if (x[i] == y[j])
-	       tab[i][j] = tab[i-1][j-1] + 1;
-	        else
-            if (tab[i-1][j] >= tab[i][j-1])
-	             tab[i][j]=tab[i-1][j];
-	              else
-                  tab[i][j]=tab[i][j-1];
-}
-}
+        for (int i = 1; i < m; i++) {
+                for (int j = 1; j < n; j++) {
+                        if (x[i] == y[j])
+                                tab[i][j] = tab[i-1][j-1] + 1;
+                        else
+                        if (tab[i-1][j] >= tab[i][j-1])
+                                tab[i][j]=tab[i-1][j];
+                        else
+                                tab[i][j]=tab[i][j-1];
+                }
+        }
 }
 
 string lcs(const string& x, const string& y){
@@ -31,7 +31,7 @@ string lcs(const string& x, const string& y){
         string ret = "";
         vector<vector<int> > mat(x.length() + 1, vector<int>(y.length() + 1, 0));
         if(!x.length() || !y.length())
-          return "";
+                return "";
 
 
 
@@ -46,14 +46,14 @@ string lcs(const string& x, const string& y){
         for(int i = x.length()-1; i >= 0; i--) {
                 for(int j = c; j >= 0; j--) {
                         here = mat[i][j];
-                        if(!here){
-                          return ret;
+                        if(!here) {
+                                return ret;
                         }
                         if(j-1 >= 0) {
                                 left = mat[i][j-1];
 
                                 if(left == here) {
-                                  cout << "gone left" << endl;
+                                        cout << "gone left" << endl;
 
                                         c--;
                                         i++;
