5eeb1c0a8311944e87bacd42a68cee0ff9699866
Daniel Carneiro <dcfreire77@gmail.com>
2019-09-09 11:00:15 -0300

    '

--- python/rsu.py
+++ python/rsu.py
@@ -1,7 +1,9 @@
 # %%
 import re
 import numpy as np
-import math
+cos = [1, 0, -1, 0]
+sin = [0, 1, 0, -1]
+
 
 class RSUProgram:
     source = ""
@@ -9,8 +11,6 @@ class RSUProgram:
     def __init__(self, source):
         self.source = source
         self.functions = {}
-
-
 
     def get_tokens(self):
         # Remove comments, sub them with " " so that stray integers may be checked correctly
@@ -35,17 +35,18 @@ class RSUProgram:
     def get_funs(self, tokens, scope="-1"):
         c = 0
         bracket = 0
-
+        saux = ['-1']
+        curp = '-1'
         while c < len(tokens):
+            # if not re.search(r"p", ''.join(tokens[c:])):
+            #    break
             if tokens[c] == '(':
                 bracket += 1
             elif tokens[c][0] == ')':
                 bracket -= 1
-            if not re.search(r"p", ''.join(tokens)):
-                break
-            if tokens[c][0] == 'p':
+            elif tokens[c][0] == 'p':
                 if bracket:
-                    raise Exception('Function nested in brackets')
+                    raise Exception()
                 bn = 1
                 bc = c
                 c += 1
@@ -57,30 +58,43 @@ class RSUProgram:
                     if c == len(tokens):
                         raise Exception('Expected function end')
                     if tokens[c][0] == 'p':
+                        curp += '|' + tokens[c][1:]
+                        saux.append(curp)
+                        if len(saux) != len(set(saux)):
+                            raise Exception()
+                        if bracket:
+                            raise Exception('Function nested in brackets')
                         bn += 1
                     elif tokens[c][0] == 'q':
+                        curp = re.sub(r"\|[^|]*$", "", curp)
                         bn -= 1
                     c += 1
+
                 if bracket:
-                    raise Exception('Brackets not oppened/closed properly in function')
+                    raise Exception(
+                        'Brackets not oppened/closed properly in function')
                 if (scope, tokens[bc][1:]) in self.functions:
                     raise Exception('Function already defined in this scope')
+
                 self.functions[(scope, tokens[bc][1:])] = tokens[bc+1:c-1]
-                
-                self.get_funs(tokens[bc+1:c-1], scope + "|" + tokens[bc][1:])
-                del tokens[bc:c]
-                c = bc - 1
+                tokens[bc] = c
+                c -= 1
             c += 1
+        return tokens
     stacktrace = []
 
     def convert_to_raw(self, tokens, scope='-1', fn='-1'):
         ret = []
-        self.get_funs(tokens)
+        if fn == '-1':
+            self.get_funs(tokens)
         c = 0
         while c < len(tokens):
-            if re.match(r"[RLF]\d+", tokens[c]):
+            if isinstance(tokens[c], int):
+                c = tokens[c]
+            elif re.match(r"[RLF]\d+", tokens[c]):
                 num = int(tokens[c][1:])
-                ret.extend([tokens[c][0] for i in range(num)])
+                for _ in range(num):
+                    ret.append(tokens[c][0])
                 c += 1
             elif re.match(r"[RLF]", tokens[c]):
                 ret.append(tokens[c])
@@ -91,11 +105,15 @@ class RSUProgram:
                     saux = re.sub(r"\|[^|]*$", "", saux)
                 if (saux, tokens[c][1:]) not in self.functions.keys():
                     raise Exception('Function not defined ' + tokens[c])
-                self.stacktrace.append((saux, tokens[c][1:]))
-                if len(set(self.stacktrace)) != len(self.stacktrace) and scope != '-1':
-                    raise Exception('Infinite loop detected in patterns')
+                if fn != '-1':
+                    self.stacktrace.append((saux, tokens[c][1:]))
+                    if len(self.stacktrace) != len(set(self.stacktrace)):
+                        raise Exception('Infinite loop detected in patterns')
+                self.get_funs(
+                    self.functions[(saux, tokens[c][1:])], saux + '|' + tokens[c][1:])
                 self.expand_fun((saux, tokens[c][1:]))
-                tokens[c:c+1] = self.functions[(saux, tokens[c][1:])]
+                ret.extend(self.functions[(saux, tokens[c][1:])])
+                c += 1
             elif tokens[c] == '(':
                 bn = 1
                 bc = c + 1
@@ -112,7 +130,8 @@ class RSUProgram:
                     bc += 1
                 c = bc
                 num = inbr[-1][1:]
-                ap = self.convert_to_raw(inbr[1:-1])
+                if num != '0':
+                    ap = self.convert_to_raw(inbr[1:-1], scope, fn)
                 if num:
                     for _ in range(int(num)):
                         ret.extend(ap)
@@ -124,10 +143,9 @@ class RSUProgram:
         return ret
 
     def expand_fun(self, key):
-        self.functions[key] = re.sub(r"p\d+.+q{1}", '', ''.join(self.functions[key]))
-        a = RSUProgram(str(self.functions[key]))
+        a = self.functions[key]
         self.functions[key] = self.convert_to_raw(
-            a.get_tokens(), key[0], key[1])
+            a, key[0], key[1])
 
     def format_grid(self, grid):
         ret = np.full(grid.shape, ' ')
@@ -137,7 +155,6 @@ class RSUProgram:
                     ret[i][j] = '*'
         return ret
 
-
     def execute_raw(self, cmds):
         grid = np.zeros((1, 1))
         grid[0][0] = '1'
@@ -145,45 +162,76 @@ class RSUProgram:
         cur_pos = [0, 0]
         for c in cmds:
             if c == 'F':
-                cur_pos[0] += round(math.sin(cur_dir))
-                cur_pos[1] += round(math.cos(cur_dir))
+                cur_pos[0] += sin[cur_dir]
+                cur_pos[1] += cos[cur_dir]
                 if cur_pos[0] < 0:
-                    grid = np.concatenate((np.zeros((1, grid.shape[1])), grid), axis=0)
+                    grid = np.concatenate(
+                        (np.zeros((1, grid.shape[1])), grid), axis=0)
                     cur_pos[0] = 0
                 elif cur_pos[1] < 0:
-                    grid = np.concatenate((np.zeros((grid.shape[0], 1)), grid), axis=1)
+                    grid = np.concatenate(
+                        (np.zeros((grid.shape[0], 1)), grid), axis=1)
                     cur_pos[1] = 0
                 if cur_pos[0] == grid.shape[0]:
-                    grid = np.concatenate((grid, np.zeros((1, grid.shape[1]))), axis=0)
+                    grid = np.concatenate(
+                        (grid, np.zeros((1, grid.shape[1]))), axis=0)
                 elif cur_pos[1] == grid.shape[1]:
-                    grid = np.concatenate((grid, np.zeros((grid.shape[0], 1))), axis=1)
+                    grid = np.concatenate(
+                        (grid, np.zeros((grid.shape[0], 1))), axis=1)
                 grid[cur_pos[0]][cur_pos[1]] = 1
 
             if c == 'R':
-                cur_dir += math.pi/2
+                cur_dir += 1
+                if cur_dir == 4:
+                    cur_dir = 0
             if c == 'L':
-                cur_dir -= math.pi/2
+                cur_dir -= 1
+                if cur_dir == -1:
+                    cur_dir = 3
         grid = self.format_grid(grid)
         ret = []
-        first = True
         for i in range(grid.shape[0]):
-            if not first:
-                ret.append("\r\n")
-            ret.extend(grid[i])
-            first = False
-        self.functions.clear()
-        return ''.join(ret)
+            ret.append(''.join(grid[i]))
+
+        return '\r\n'.join(ret)
 
     def execute(self):
-        ret = self.execute_raw(self.convert_to_raw(self.get_tokens()))
-        self.stacktrace.clear()
-        self.functions.clear()
-        return ret
+        return self.execute_raw(self.convert_to_raw(self.get_tokens()))
 
 
-RSUProgram("""
-p0FFLFFR((FFFR)2(FFFFFL)3)4qp1FRqp2FP1qp3FP2qp4FP3qP0P1P2P3P4
-FFLFFR((FFFR)2(FFFFFL)3)4""").execute()
-
-
+RSUProgram.execute(RSUProgram(
+    """(F2F6)8P399(FF(LRP684F)(F9P546R8F)4P546)FF6RP684FP684
+p399
+    p546
+        p399
+            RL3
+        q
+        p399
+            L7LR5R8L
+        q
+    q
+F(F6L(FFL8L)R)L4
+q
+p546
+    F6RL4FR5L0(FLFF9)F
+    p590
+        P546LF9RF1
+        p684
+            p590
+            q
+        q
+    q
+q
+LR
+p73
+    F1L6
+q
+F
+p684
+    ((F))1LF
+    p936
+    R(F2R)1RRRRLP684
+    q
+    (RL(F7R)F)
+q"""))
 # %%
